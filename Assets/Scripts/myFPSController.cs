8b386e9b01a31143ce553b2528baf62e12e52a73
LukeSkypewalker <work37@gmail.com>
2017-01-15 17:00:34 +0300

    +FPS controller

---
+++ Assets/Scripts/myFPSController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class myFPSController : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public Gun gun;
+    public int playerNumber;
+    public float sensitivity = 5.0f;
+
+    void Start () {
+	    Cursor.lockState = CursorLockMode.Locked;
+	}
+
+    void Update()
+    {
+        float forwardMove = Input.GetAxis("Vertical_p" + playerNumber) * moveSpeed * Time.deltaTime;
+        float strafe = Input.GetAxis("Horizontal_p" + playerNumber) * moveSpeed * Time.deltaTime;
+
+        transform.Translate(strafe, 0, forwardMove);
+
+        float isFired = Input.GetAxis("Fire1_p" + playerNumber);
+        if (isFired > 0 && gun.isReadyToShoot())
+            gun.Fire();
+
+        float mouseH = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseV = Input.GetAxis("Mouse Y") * sensitivity;
+
+        Camera.main.transform.Rotate(-mouseV,0,0);
+        this.transform.Rotate(0, mouseH, 0);
+
+    }
+}
+
